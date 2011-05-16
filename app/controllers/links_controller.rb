6b70e1fde38f742b990995d08175c8d4250c9edb
Tony Hillerson <tony.hillerson@gmail.com>
2011-05-16 15:05:23 -0600

    Fixing a bug in the links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,7 @@ class LinksController < ApplicationController
   respond_to :html, :json
 
   def create
-    link = Link.create :id => Link.new_id.to_s(36), :link => params[:link]
+    link = Link.create :id => Link.new_id, :link => params[:link]
     respond_with(link, :location => short_code_path(link.id))
   end
 
